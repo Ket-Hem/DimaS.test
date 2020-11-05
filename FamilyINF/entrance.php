c2dd62c87150224bfcef4ada90363c9337151ff9
Dmytro <dimasayjk@gmail.com>
2020-11-05 19:26:28 +0200

    Second commit for test purpose

---
+++ FamilyINF/entrance.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TestForFunc</title>
+</head>
+<body>
+<h3>Hello owner! Nice to see you. Keep working! :D</h3>
+<?php
+//echo "Сьогодні: ".date("d");
+
+
+
+
+?>
+</body>
+</html>
