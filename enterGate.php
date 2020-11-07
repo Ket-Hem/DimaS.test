812ee41a208a825334fee465741cfefcc9b8b172
Dmytro <dimasayjk@gmail.com>
2020-11-07 13:01:39 +0200

    Finished head with additional <meta> lines

---
+++ enterGate.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="This page is going to become a entrance to my first-created site">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Ket-Hem">
+    <title>EnterGate</title>
+</head>
+<body>
+    <h3>Hello owner! Nice to see you. Keep working! :D</h3>
+    <!-- Форма авторизації -->
+    <?php
+
+
+
+
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>TestForFunc</title>
+    <title>Index</title>
 </head>
 <body>
 <h3>Hello owner! Nice to see you. Keep working! :D</h3>
