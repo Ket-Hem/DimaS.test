077f7c4b5d041bef897089fa49b8928ba4d63d4a
Dmytro <dimasayjk@gmail.com>
2020-11-08 23:37:08 +0200

    - create CSS style for page - find out how to add a image

--- enterGate.php
+++ enterGate.php
@@ -5,11 +5,43 @@
     <meta name="description" content="This page is going to become a entrance to my first-created site">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Ket-Hem">
+    <!--<link rel="StyleSheet" href="EnterGate.css">
+    External file of CSS does not work, i have to put it inside *.php file-->
+    <style>
+        h1 {
+            background-color: greenyellow;
+            border: 5px dotted purple;
+            color: navy;
+            text-align: center;
+        }
+        body {
+            background-color:hsla(70, 97%, 55%, 0.4);
+            border: 3px solid rgba(255, 255, 0, 1);
+            padding: 20px;
+            margin: 50px;
+            color: #363636;
+            text-align: center;
+            font-family: Calibri;
+            font-size: 150%;
+        }
+    </style>
     <title>EnterGate</title>
 </head>
+<!-- Забрати потім колір фону сторінк і зробити body в CSS-->
 <body>
-    <h3>Hello owner! Nice to see you. Keep working! :D</h3>
-    <!-- Форма авторизації -->
+    <h1>Hello owner! Nice to see you. Keep working! :D</h1>
+
+    <pre>
+        It matters not how strait the gate,
+        How charged with punishments the scroll,
+        I am the master of my fate:
+        I am the captain of my soul.</pre>
+
+    <!-- Картинка вітряка-->
+    <img src="https://cdn.pixabay.com/photo/2020/11/04/19/22/windmill-5713337_960_720.jpg" alt="A windmill" width="500" height="400">
+    <!--<img src="https://drive.google.com/drive/u/0/folders/0B4wdOSbuqKGJNnBMYUhhQnNZazQ/85.jpg" alt="Florence temple" width="500" legth="400"> -->
+
+
     <?php
 
 
