c872a3cec5f977ef41f2431f86014d0e7a68332a
Dmytro <dimasayjk@gmail.com>
2021-01-24 23:44:57 +0200

    reinstall xampp

--- index.php
+++ index.php
@@ -6,14 +6,15 @@
 </head>
 <body>
 <h3>Hello owner! Nice to see you. Keep working! :D</h3>
-
-
-
-
-
 <?php
-
-
+echo date('jS l \of F Y H:i:s')."|||";
+$year=2020;
+$leap=date("L" , mktime(0, 0, 0, 0, 0, $year));
+if($leap==TRUE){
+    echo "The year $year is the leap year";
+    }else{
+    echo "The year $year is NOT the leap year";
+    }
 ?>
 </body>
 </html>
